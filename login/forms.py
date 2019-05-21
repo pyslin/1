ff78569270819a865fe5ffe3c580fab5352d71e5
pyslin <xingkemo@icloud.com>
2019-05-21 21:18:35 +0800

    注册视图 密码加盐

--- login/forms.py
+++ login/forms.py
@@ -1,7 +1,9 @@
+#顶部要先导入forms模块所有的表单类都要继承forms.Form类
 from django import forms
 #验证码
 from captcha.fields import CaptchaField
-#顶部要先导入forms模块所有的表单类都要继承forms.Form类
+
+
 class UserForm(forms.Form):
     username = forms.CharField(label="用户名", max_length=128, widget=forms.TextInput(attrs={'class': 'form-control',
                                                                                           'placeholder': "Username",'autofocus': ''}))
@@ -9,3 +11,15 @@ class UserForm(forms.Form):
                                                                                              'placeholder': "Password"}))
     captcha = CaptchaField(label='验证码')
 #widget=forms.PasswordInput用于指定该字段在form表单里表现为<input type='password' />，也就是密码输入框
+
+class RegisterForm(forms.Form):
+    gender = (
+        ('male', "男"),
+        ('female', "女"),
+    )
+    username = forms.CharField(label="用户名", max_length=128, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password1 = forms.CharField(label="密码", max_length=256, widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    password2 = forms.CharField(label="确认密码", max_length=256, widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(label="邮箱地址", widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    sex = forms.ChoiceField(label='性别', choices=gender)
+    captcha = CaptchaField(label='验证码')
--- login/views.py
+++ login/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
+#引入表单 模型
 from . import models
 from . import forms
+import hashlib
 
 # Create your views here.
+def hash_code(s, salt='mysite'):# 加点盐
+    h = hashlib.sha256()
+    s += salt
+    h.update(s.encode())  # update方法只接收bytes类型
+    return h.hexdigest()
+
+
 def index(request):
-    pass
+    #session未登录限制访问的代码
+    if not request.session.get('is_login', None):
+        return redirect('/login/')
     return render(request, 'login/index.html')
 """
 def login(request):
@@ -44,6 +55,9 @@ def login(request):
 #然后作为第三个参数提供给render方法。这个数据字典在渲染模板的时候会传递到模板里供你调用。
 """
 def login(request):
+    # 不允许重复登录
+    if request.session.get('is_login', None):
+        return redirect('/index/')
     #先实例化，不然出不了输入框
     login_form = forms.UserForm()
     if request.method == 'POST':
@@ -59,7 +73,11 @@ def login(request):
                 message = '用户不存在！'
                 return render(request, 'login/login.html', locals())
 
-            if user.password == password:
+            if user.password == hash_code(password):
+                #往session字典内写入用户状态和数据：
+                request.session['is_login'] = True
+                request.session['user_id'] = user.id
+                request.session['user_name'] = user.name
                 return redirect('/index/')
             else:
                 message = '密码不正确！'
@@ -69,9 +87,50 @@ def login(request):
     return render(request, 'login/login.html', locals())
 
 def register(request):
-    pass
-    return render(request, 'login/register.html')
+    if request.session.get('is_login', None):
+        return redirect('/index/')
+
+    if request.method == 'POST':
+        register_form = forms.RegisterForm(request.POST)
+        message = "请检查填写的内容！"
+        if register_form.is_valid():
+            username = register_form.cleaned_data.get('username')
+            password1 = register_form.cleaned_data.get('password1')
+            password2 = register_form.cleaned_data.get('password2')
+            email = register_form.cleaned_data.get('email')
+            sex = register_form.cleaned_data.get('sex')
+
+            if password1 != password2:
+                message = '两次输入的密码不同！'
+                return render(request, 'login/register.html', locals())
+            else:
+                same_name_user = models.User.objects.filter(name=username)
+                if same_name_user:
+                    message = '用户名已经存在'
+                    return render(request, 'login/register.html', locals())
+                same_email_user = models.User.objects.filter(email=email)
+                if same_email_user:
+                    message = '该邮箱已经被注册了！'
+                    return render(request, 'login/register.html', locals())
+
+                new_user = models.User()
+                new_user.name = username
+                new_user.password = hash_code(password1)
+                new_user.email = email
+                new_user.sex = sex
+                new_user.save()
+
+                return redirect('/login/')
+        else:
+            return render(request, 'login/register.html', locals())
+    register_form = forms.RegisterForm()
+    return render(request, 'login/register.html', locals())
+"""重点在于注册逻辑，首先两次输入的密码必须相同，其次不能存在相同用户名和邮箱，
+最后如果条件都满足，利用ORM的API，创建一个用户实例，然后保存到数据库内"""
 
 def logout(request):
-    pass
+    # 如果本来就未登录，也就没有登出一说
+    if not request.session.get('is_login', None):
+        return redirect('/login/')
+    request.session.flush()
     return redirect('/login/')
